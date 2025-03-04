6b633e5f7c28164273bbbfbfe7b1db5912bd1f01
OmarCanton <146691481+OmarCanton@users.noreply.github.com>
2025-03-04 01:00:45 +0000

    Update main.jsx

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -1,7 +1,7 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter as Router } from 'react-router-dom'
 import App from './App.jsx'
-import store from './Redux/Store/store.jsx'
+import store from './Redux/Store/Store.jsx'
 import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
