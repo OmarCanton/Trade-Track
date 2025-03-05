e90d921d18576f33a6cf096f62bc475b77909154
OmarCanton <146691481+OmarCanton@users.noreply.github.com>
2025-03-05 12:51:09 +0000

    Update ContactAdmin.jsx

--- Client/src/Pages/ContactAdmin/ContactAdmin.jsx
+++ Client/src/Pages/ContactAdmin/ContactAdmin.jsx
@@ -1,4 +1,17 @@
+import { useNavigate } from "react-router-dom"
+import { UserCredsContext } from "../../Context/UserCredsContext"
+import { useContext, useEffect } from "react"
+
 export default function ContactAdmin() {
+    const navigate = useNavigate()
+    const {haveAccess} = useContext(UserCredsContext)
+    useEffect(() => {
+        if(haveAccess) {
+            navigate('/')
+        } else {
+            return
+        }
+    }, [haveAccess, navigate])
     return (
         <h1>Please contact your Boss for access to this App</h1>
     )
